e336697725c4105d27787aab0d327aea57df4fea
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-22 19:06:03 +0530

    Create Store.js

---
+++ Pages/Store.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Header2 from "./Header2";
+function Home(){
+    return(
+<div>
+    <Header/>
+    <Header2/>
+    <Footer/>
+</div>
+    );
+}
+export default Home;
