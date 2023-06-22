58aeb908aa94b28b1a44a316299c1e2303c5a7ef
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-22 15:16:59 +0530

    Update Article.js

--- Article.js
+++ Article.js
@@ -14,6 +14,7 @@ function Article(){
         </h6>
         <button><a href="https://www.starbucks.in/Blogs"  style={{textDecoration:'none',fontFamily:'kanit'}}>Learn More</a></button>
     </div>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime et sint laboriosam ullam reiciendis officiis quia veniam voluptatum expedita nesciunt maiores explicabo animi commodi deleniti, iusto ipsum, id dignissimos placeat. Tempore impedit blanditiis, quis voluptates laborum animi optio saepe natus facilis, earum recusandae veritatis odio exercitationem nam voluptatem. Exercitationem, rerum dolorem modi deleniti possimus mollitia sequi optio officia earum libero fugit ipsam, sint voluptatum nostrum nobis eveniet fugiat error officiis minus. Similique molestiae atque provident molestias a ullam cupiditate veritatis repudiandae tempore deserunt, eius dicta architecto eos maiores placeat! Quidem accusantium nobis tempore earum provident, quos eos nihil repudiandae reprehenderit?</p>
     </div>
     );
 }
