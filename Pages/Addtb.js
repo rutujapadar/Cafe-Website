2076ac3fb50366ff517cd4adfb63bbbc8a78d42c
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-22 19:04:35 +0530

    Create Addtb.js

---
+++ Pages/Addtb.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Header from './Header';
+import Footer from './Footer';
+import Header3 from "./Header3";
+
+function Addtb() {
+  return (
+    <div>
+        <Header/>
+        <Header3/>
+        <div>
+          
+        </div>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          
+        <Footer/>
+    </div>
+  );
+}
+
+export default Addtb;
