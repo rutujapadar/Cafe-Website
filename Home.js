867f6c6050f493e29dbf8fa33e3715848ee9a0b9
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-22 12:40:19 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Circles from "./Circles";
+import Article from "./Article";
+import Footer from "./Footer";
+
+function Home(){
+    return(
+<div>
+    <Header/>
+    <Circles/>
+    <Article/>
+    <Footer/>
+</div>
+    );
+}
+export default Home;
