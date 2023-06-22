949f2ca5ec618c82a7e06744adf4704f23139d35
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-22 12:36:37 +0530

    Create Article.js

---
+++ Article.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Article(){
+    return(
+    <div className="ArticleCon">
+         <h1>Learn More About World Of Coffee</h1>
+    <div className="Art">
+       
+        <h2>
+            Art of coffee making and its trend
+        </h2>
+        <h6>
+            explore the world of coffee and our finest makers in india
+        </h6>
+        <button><a href="https://www.starbucks.in/Blogs"  style={{textDecoration:'none',fontFamily:'kanit'}}>Learn More</a></button>
+    </div>
+    </div>
+    );
+}
+export default Article;
