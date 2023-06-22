61ff2e49613ea58f1adf91e16bd3755186d7d9b9
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-22 12:37:55 +0530

    Create Order.js

---
+++ Order.js
@@ -0,0 +1,282 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import im1 from './img/tang.JPG';
+import im2 from './img/cappu.JPG';
+import im3 from './img/taco.JPG';
+import icon from './img/veg.png'
+function Order(){
+    return(
+<div>
+    <div className="MenuCon">
+        <Header/>
+     <div className="Menu">
+<div className="Cards">
+    <div className="info">
+        <img src={im1}></img>
+        <div className="hehe">
+           
+            <h2>Citrus ale</h2>
+            <h6>250ml (400kcal)</h6>
+            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+        </div>
+        <div className="cart">
+            <h4>$200</h4>
+            <button>Add</button>
+        </div>
+    </div>
+    
+    </div>
+<div className="Cards">
+    <div className="info">
+<img src={im2}></img>
+<div className="hehe">
+           
+           <h2>Cappucino</h2>
+           <h6>Per Serve 160g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$150</h4>
+           <button>Add</button>
+       </div>
+</div>
+</div>
+<div className="Cards">
+    <div className="info">
+    <img src={im3}></img>
+    <div className="hehe">
+           
+           <h2>Mexican Tacos</h2>
+           <h6>Per Serve 200g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$20</h4>
+           <button>Add</button>
+       </div>
+        </div>
+        
+     </div>
+
+     </div>
+
+     <div className="Menu2">
+<div className="Cards">
+    <div className="info">
+        <img src={im1}></img>
+        <div className="hehe">
+           
+            <h2>Citrus ale</h2>
+            <h6>250ml (400kcal)</h6>
+            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+        </div>
+        <div className="cart">
+            <h4>$200</h4>
+            <button>Add</button>
+        </div>
+    </div>
+    
+    </div>
+<div className="Cards">
+    <div className="info">
+<img src={im2}></img>
+<div className="hehe">
+           
+           <h2>Cappucino</h2>
+           <h6>Per Serve 160g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$150</h4>
+           <button>Add</button>
+       </div>
+</div>
+</div>
+<div className="Cards">
+    <div className="info">
+    <img src={im3}></img>
+    <div className="hehe">
+           
+           <h2>Mexican Tacos</h2>
+           <h6>Per Serve 200g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$20</h4>
+           <button>Add</button>
+       </div>
+        </div>
+        
+    </div>
+
+     </div>
+
+     <div className="Menu2">
+<div className="Cards">
+    <div className="info">
+        <img src={im1}></img>
+        <div className="hehe">
+           
+            <h2>Citrus ale</h2>
+            <h6>250ml (400kcal)</h6>
+            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+        </div>
+        <div className="cart">
+            <h4>$200</h4>
+            <button>Add</button>
+        </div>
+    </div>
+    
+    </div>
+<div className="Cards">
+    <div className="info">
+<img src={im2}></img>
+<div className="hehe">
+           
+           <h2>Cappucino</h2>
+           <h6>Per Serve 160g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$150</h4>
+           <button>Add</button>
+       </div>
+</div>
+</div>
+<div className="Cards">
+    <div className="info">
+    <img src={im3}></img>
+    <div className="hehe">
+           
+           <h2>Mexican Tacos</h2>
+           <h6>Per Serve 200g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$20</h4>
+           <button>Add</button>
+       </div>
+        </div>
+        
+    </div>
+
+     </div>
+
+
+<div className="types">
+<h3>-------Beverages-------</h3>
+</div>
+
+
+     <div className="Menu3">
+
+<div className="Cards">
+    <div className="info">
+        <img src={im1}></img>
+        <div className="hehe">
+           
+            <h2>Citrus ale</h2>
+            <h6>250ml (400kcal)</h6>
+            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+        </div>
+        <div className="cart">
+            <h4>$200</h4>
+            <button>Add</button>
+        </div>
+    </div>
+    
+    </div>
+<div className="Cards">
+    <div className="info">
+<img src={im2}></img>
+<div className="hehe">
+           
+           <h2>Cappucino</h2>
+           <h6>Per Serve 160g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$150</h4>
+           <button>Add</button>
+       </div>
+</div>
+</div>
+<div className="Cards">
+    <div className="info">
+    <img src={im3}></img>
+    <div className="hehe">
+           
+           <h2>Mexican Tacos</h2>
+           <h6>Per Serve 200g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$20</h4>
+           <button>Add</button>
+       </div>
+        </div>
+        
+    </div>
+
+     </div>
+
+     <div className="Menu4">
+<div className="Cards">
+    <div className="info">
+        <img src={im1}></img>
+        <div className="hehe">
+           
+            <h2>Citrus ale</h2>
+            <h6>250ml (400kcal)</h6>
+            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+        </div>
+        <div className="cart">
+            <h4>$200</h4>
+            <button>Add</button>
+        </div>
+    </div>
+    
+    </div>
+
+<div className="Cards">
+    <div className="info">
+<img src={im2}></img>
+<div className="hehe">
+           
+           <h2>Cappucino</h2>
+           <h6>Per Serve 160g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$150</h4>
+           <button>Add</button>
+       </div>
+</div>
+</div>
+<div className="Cards">
+    <div className="info">
+    <img src={im3}></img>
+    <div className="hehe">
+           
+           <h2>Mexican Tacos</h2>
+           <h6>Per Serve 200g (400kcal)</h6>
+           <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
+       </div>
+       <div className="cart">
+           <h4>$20</h4>
+           <button>Add</button>
+       </div>
+        </div>
+        
+    </div>
+
+     </div>
+
+
+           <Footer/>
+    </div>
+</div>
+    );
+}
+export default Order;
