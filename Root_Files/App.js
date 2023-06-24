7f8e8eccee846fe6f88248e44e30b5f2002c8d53
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-24 09:30:25 +0530

    Update App.js

--- Root_Files/App.js
+++ Root_Files/App.js
@@ -6,19 +6,30 @@ import Order from './Components/Order';
 import Store from './Components/Store';
 import Formm from './Components/Formm';
 import Addtb from './Components/Addtb';
+
+
+import ScrollButton from "./Components/Scrollbutton";
+import { Content, Heading } from './Components/Style';
+
+
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Order' element={<Order/>}/>
-      <Route path='/Store' element={<Store/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Formm' element={<Formm/>}/>
-      <Route path='/Addtb' element={<Addtb/>}/>
-  
-    </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Order' element={<Order/>}/>
+        <Route path='/Store' element={<Store/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Formm' element={<Formm/>}/>
+        <Route path='/Addtb' element={<Addtb/>}/>
+      </Routes>
+      </BrowserRouter>
+      
+   
+      <ScrollButton />
+     
+    </div>
   );
 }
 
