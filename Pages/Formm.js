8442f8e8c1673153d34139967a77bc47d34204e3
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-06-29 12:36:43 +0530

    Update Formm.js

--- Pages/Formm.js
+++ Pages/Formm.js
@@ -1,30 +1,46 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import Header2 from './Header2'
+// import Header2 from './Header2'
+import Header3 from './Header3'
+import { useState } from 'react'
 
 export default function Formm() {
+
+  const [[name, setName], [email, setEmail], [contact, setContact]] = [
+    useState('Byul'), useState("vaisgheh"),useState("0987823749")];
+  
+    function getData(e){
+      let data=name;
+      let data2=email;
+      let data3=contact;
+      e.preventDefault();
+      
+      console.log(data,data2,data3);
+    }
   return (
     <div>
 
         <Header/>
-        <Header2/>
+        <Header3/>
         <div className='formbg'>
           <h3>Welcome To Bella Italia</h3>
-          <form>
+          <form  onSubmit={getData}>
             <h2>Register</h2>
-            <label htmlFor="fname">Username</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
-            <label htmlFor="lname">Email</label>
+            <label htmlFor="name">Full Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.."  onChange={(e) => setName(e.target.value)}
+        />
+            <label htmlFor="email">Email</label>
             <input
-              type="text"
-              id="lname"
-          
-              placeholder="Email.."
-            />
-            <label htmlFor="fname">Password</label>
-            <input type="text" id="fname" name="firstname" placeholder="Password.." />
-            <input type="submit" defaultValue="Submit" />
+              type="email"
+              id="email"placeholder="Email.."  onChange={(e) => setEmail(e.target.value)
+              }
+              />
+            <label htmlFor="contact">Contact</label>
+            <input type='number' id="fname" name="firstname" maxlength="10" placeholder="contact.."  onChange={(e) => setContact(e.target.value)}
+       />
+ 
+            <input type="submit" defaultValue="Submit"/>
           </form>
         </div>
         <Footer/>
