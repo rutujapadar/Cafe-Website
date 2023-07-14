e48533c39497a47890a39bafd1839fa4a0dbcc56
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-07-14 14:15:29 +0530

    Update Addtb.js

--- Pages/Addtb.js
+++ Pages/Addtb.js
@@ -1,53 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import Header from './Header';
 import Footer from './Footer';
 import Header3 from "./Header3";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import im1 from './img/cappu.JPG'
+import Services from "./Services";
 
 function Addtb() {
+  const [data,setUser]=useState([]);
+   
+
+  //  useEffect(()=>{
+  //   //fetch("https://jsonplaceholder.typicode.com/users/1")
+  //   fetch("/getOrder")
+  //   .then((result)=>result.json())
+  //   .then((data)=>setUser(data));
+  //       },[] 
+  //  )
+  // "proxy": "http://localhost:8005",
+
+  useEffect(()=>{
+    fetch("/getUser")
+    .then((res)=>res.json())
+    .then((data)=>setUser(data.data))
+  },[])
+  
   return (
-    <div>
+    <div >
         <Header/>
         <Header3/>
-        <div>
-          
+        <h3 style={{fontFamily:'kanit',marginLeft:'70px',textDecoration:'underline'}}>
+      Order Details
+        </h3>
+        <div className='cartTitle'>
+          <h4>Item</h4>
+          <h4>Quantity</h4>
+          <h4>Price</h4>
+          <h4>Total</h4>
         </div>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          
+        <div>
+          {data.map((dataObj,index)=>{
+            return (
+              <Card sx={{ maxWidth:750,display:'flex',height:"100px",margin:"35px 60px"}}>
+              
+              <CardMedia
+                sx={{ width:100,height:"80px",margin:"10px"}}
+                image={im1} />
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Typography gutterBottom variant="h6" component="div">
+                   {dataObj.name}
+                  </Typography>
+                  <Typography variant="body3" color="text.secondary" >
+                   {dataObj.city}
+                  </Typography>
+                  
+                </CardContent>
+              </Box>
+              <CardActions>
+              
+                <Button size="small">-
+                </Button>
+                <Typography variant="body1" color="text.primary">
+                 1
+                  </Typography>
+                <Button size="small" >+</Button>
+                
+              </CardActions>
+              <CardContent sx={{marginTop:'20px'}}>
+              <Typography variant="body1" color="text.secondary">
+                  ₹{dataObj.contact}
+                  </Typography>
+             
+              </CardContent>
+              <CardContent sx={{marginTop:'20px',marginLeft:'50px'}}>
+              <Typography variant="body1" color="text.primary">
+                  ₹{dataObj.contact}
+                  </Typography>
+             
+              </CardContent>
+            </Card>
+            )
+          })}
+       
+        </div>
+         <br></br>
+         <br></br>
+         <br></br>
+         <br></br>
+         <br></br>
+         {/* <Services/> */}
         <Footer/>
     </div>
   );
