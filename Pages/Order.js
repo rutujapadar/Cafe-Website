728d36bb566afff1c78ecfa26d72e1bdd144a027
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-07-16 18:43:07 +0530

    Update Order.js

--- Pages/Order.js
+++ Pages/Order.js
@@ -1,42 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import im1 from './img/tang.JPG';
-import im2 from './img/cappu.JPG';
-import im3 from './img/taco.JPG';
-import im4 from './img/mocha.JPG';
-import im5 from './img/americano.jpg';
-import im6 from './img/bananan.jpg';
-import im7 from './img/blueshake.jpeg';
-import im8 from './img/cheesecake.JPG';
-import im9 from './img/chocoshake.jpeg';
-import im10 from './img/croissant.JPG';
-import im11 from './img/donut.jpeg';
-import im12 from './img/frappe.jpg';
-import im13 from './img/fries.jpeg';
-import im14 from './img/garlicbread.jpeg';
-import im15 from './img/icedtea.jpeg';
-import im16 from './img/italian.jpeg';
-import im17 from './img/latte.webp';
-import im18 from './img/loafcake.jpg';
-import im19 from './img/mac.jpg';
-import im20 from './img/macaroi.jpeg';
-import im21 from './img/marghe.jpeg';
-import im22 from './img/muffins.jpeg';
-import im23 from './img/pesto.jpg';
-import im24 from './img/pancake.jpeg';
-import im25 from './img/redvelvet.jpeg';
-import im27 from './img/toast.JPG';
-import im28 from './img/aloopuff.jpg';
-import im29 from './img/classic.jpg';
-import im30 from './img/pasta.jpeg';
-import im31 from './img/mojito.jpeg'
+
 import Header2 from "./Header2";
-import Axios from 'axios';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect,useState } from "react";
-
+import cardData from "./cardData";
+import {useDispatch} from "react-redux"
+import{ ADD }from "../redux/actions/action"
 // import { Card } from 'react-card-component';
 
 function Order() {
@@ -44,145 +23,135 @@ function Order() {
     // const [[name, setName], [price, setPrice], [qty, setQty]] = [
     //     useState(""), useState(""), useState("1")];
 
-        const [productDetails,setProductDetails] = useState({
-            name:"",
-            price:"",
-            qty:"",
-            desc:""
-        })
+        // const [productDetails,setProductDetails] = useState({
+        //     name:"",
+        //     price:"",
+        //     qty:"",
+        //     desc:""
+        // })
         
-        function setvj (){
-            // setName("peach iced tea");
-            // setPrice(230)
-            setProductDetails({ name:"mocha",price:130,qty:1,desc:"250ml serves 1 (250kcal)"})
-        }
-        function setp(){
-            // setName("peach iced tea");
-            // setPrice(230)
-            setProductDetails({ name:"iced tea",price:180,qty:1,desc:"250ml serves 1 (250kcal)"})
-        }
-        function setv (){
-            // setName("peach iced tea");
-            // setPrice(230)
-            setProductDetails({ name:"cappu",price:230,qty:1,desc:"250ml serves 1 (250kcal)"})
-        }
-        function addCart(){                         
-        let data1 = productDetails.name;
-        let data2 = productDetails.price;   
-        let data3 = productDetails.qty;
-        let data4 = productDetails.desc;
+        // function setvj (){
+        //     // setName("peach iced tea");
+        //     // setPrice(230)
+        //     setProductDetails({ name:"mocha",price:130,qty:1,desc:"250ml serves 1 (250kcal)"})
+        // }
+        // function setp(){
+        //     // setName("peach iced tea");
+        //     // setPrice(230)
+        //     setProductDetails({ name:"iced tea",price:180,qty:1,desc:"250ml serves 1 (250kcal)"})
+        // }
+        // function setv (){
+        //     // setName("peach iced tea");
+        //     // setPrice(230)
+        //     setProductDetails({ name:"cappucino",price:230,qty:1,desc:"250ml serves 1 (250kcal)"})
+        // }
+        // function addCart(){                         
+        // let data1 = productDetails.name;
+        // let data2 = productDetails.price;   
+        // let data3 = productDetails.qty;
+        // let data4 = productDetails.desc;
 
-        //mongo 
-        let info = JSON.stringify({
-            name:data1,
-            price:data2,
-            qty:data3,
-            desc:data4
-          });
-           const ops = {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            data:info ,
-            url:"/addCart"
-            };
-           Axios(ops)
-           .then((res)=>{
-            if(res.status==200){
-              toast("Item added to Cart!",{position: toast.POSITION.BOTTOM_CENTER},{autoClose:1000})
-            }
-           })
+        // //mongo 
+        // let info = JSON.stringify({
+        //     name:data1,
+        //     price:data2,
+        //     qty:data3,
+        //     desc:data4
+        //   });
+        //    const ops = {
+        //     method: 'POST',
+        //     headers: { 'content-type': 'application/json' },
+        //     data:info ,
+        //     url:"/addCart"
+        //     };
+        //    Axios(ops)
+        //    .then((res)=>{
+        //     if(res.status==200){
+        //       toast("Item added to Cart!",{position: toast.POSITION.BOTTOM_CENTER},{autoClose:1000})
+        //     }
+        //    })
 
-        //mySQL 
-        // const requestOptions = {
-        //     method: 'POST', 
-        //     mode: 'cors' ,         
-        //     headers: {  
-        //         'Content-Type': 'application/json',
-        //         'accept': 'application/json'
-        //     },           
-        //     body: JSON.stringify({ name: data, price: data1, quantity: data2 })
-        // };
-        // fetch('/addcart', requestOptions)
-        //     .then((response) => {
-        //         if (response.status == 200) {
-        //             response.json();
-        //             alert("ok")
-        //         }               
+        // //mySQL 
+        // // const requestOptions = {
+        // //     method: 'POST', 
+        // //     mode: 'cors' ,         
+        // //     headers: {  
+        // //         'Content-Type': 'application/json',
+        // //         'accept': 'application/json'
+        // //     },           
+        // //     body: JSON.stringify({ name: data, price: data1, quantity: data2 })
+        // // };
+        // // fetch('/addcart', requestOptions)
+        // //     .then((response) => {
+        // //         if (response.status == 200) {
+        // //             response.json();
+        // //             alert("ok")
+        // //         }               
                 
-        //     })
+        // //     })
            
             
                         
                         
-                    }
-                    
-                    
-                    
+        //             }
+         const [data,setData]=useState(cardData);        
+         
+
+        const dispatch = useDispatch()
+
+
+
+        const send=(e)=>{
+
+            dispatch(ADD(e));
+        }
     return (
         
         <div className="MenuCon">
             <Header />
             <Header2 />
             <div className="types">
-                <h3>-------Beverages-------</h3>
+                <h3>-------MENU-------</h3>
             </div>
-            <div className="Menu">
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im1}></img>
-                        <div className="hehe" id="heh ">
-
-                            <h2>Peach Iced Tea</h2>
-                            <h6>250ml (400kcal)</h6>
-                            <h6>Incredibly simple 4-ingredient peach iced tea infused with real peaches and peach simple syrup. The perfect summer drink to sip by the pool.</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>₹150</h4>
-
-                            <button onClick={()=>{setp();addCart()}}>Add</button>
-                            <ToastContainer />
-
-
-                        </div>
+            {data.map((dataObj)=>{
+                return(
+                    <div style={{display:"inline-block",marginTop:"-3%",marginBottom:"7%"}}>
+                        <Card sx={{ maxWidth:300,height:"320px",marginLeft:"90px",marginRight:"10px" ,backgroundColor:"#dadada6c"}}>
+                        <CardMedia
+                          sx={{ width:275,height:"150px",margin:"10px"}}
+                          image={dataObj.img} />
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                          <CardContent sx={{ flex: '1 0 auto' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' ,}}>
+                                <Typography gutterBottom variant="h6" component="div" style={{marginTop:"-8%"}}>
+                                 {dataObj.title}
+                                </Typography>
+                                <Typography variant="body3" color="text.secondary" sx={{marginTop:"-6%",marginLeft:"5%"}}>
+                                 {dataObj.quantity}
+                                </Typography>
+                            </Box>
+                            <Typography variant="body3" color="text.secondary" >
+                             {dataObj.desc}
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography variant="body1" color="text.primary" className="cardprice" style={{marginTop:"10%"}}>
+                                ₹{dataObj.price}
+                                </Typography>
+                                <Button className="cardbutton" style={{marginLeft:"50%",marginTop:"8%", backgroundColor:"#944e32",color:"white"}} onClick={()=>send(dataObj)}>ADD</Button>
+                            </Box>
+                          </CardContent>
+                        </Box>
+                        <CardContent sx={{marginTop:'20px'}}>
+                        
+                        </CardContent>
+                                           
+                                          </Card>
                     </div>
 
-                </div>
-                {/* <Card><div>HelloWorld</div></Card> */}
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im2}></img>
-                        <div className="hehe">
+                )
+            })}
 
-                            <h2>Cappucino</h2>
-                            <h6>250ml (400kcal)</h6>
-                            <h6>A cappuccino is an Italian coffee drink that is traditionally prepared with equal parts double espresso, steamed milk, and steamed milk foam on top. </h6>
-                        </div>
-                        <div className="cart">
-                            <h4>₹109</h4>
-                            <button onClick={()=>{setv(); addCart()}}>Add</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im4}></img>
-                        <div className="hehe">
-
-                            <h2>Mocha</h2>
-                            <h6>200ml (400kcal)</h6>
-                            <h6> caffè mocha is based on espresso and hot milk but with added chocolate flavouring and sweetener, typically in the form of cocoa powder and sugar. </h6>
-                        </div>
-                        <div className="cart">
-                            <h4>₹129</h4>
-                            <button onClick={()=>{setvj(); addCart()}}>Add</button>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
+            {/* 
             <div className="Menu2">
                 <div className="Cards">
                     <div className="info">
@@ -335,66 +304,12 @@ function Order() {
 
                 </div>
 
-            </div>
+            </div> */}
 
-            <div className="types1">
-                <h3>-------Food-------</h3>
-            </div>
+        
 
 
-            <div className="Menu3">
-
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im13}></img>
-                        <div className="hehe">
-
-                            <h2>Citrus ale</h2>
-                            <h6>250ml (400kcal)</h6>
-                            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>$200</h4>
-                            <button>Add</button>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im14}></img>
-                        <div className="hehe">
-
-                            <h2>Cappucino</h2>
-                            <h6>Per Serve 160g (400kcal)</h6>
-                            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>$150</h4>
-                            <button>Add</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im16}></img>
-                        <div className="hehe">
-
-                            <h2>Mexican Tacos</h2>
-                            <h6>Per Serve 200g (400kcal)</h6>
-                            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>$20</h4>
-                            <button>Add</button>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="Menu4">
+            {/* <div className="Menu4">
                 <div className="Cards">
                     <div className="info">
                         <img src={im3}></img>
@@ -548,66 +463,13 @@ function Order() {
 
                 </div>
 
-            </div>
+            </div> */}
 
 
-            <div className="types1">
-                <h3>-------Desserts-------</h3>
-            </div>
+          
 
-            <div className="Menu3">
 
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im8}></img>
-                        <div className="hehe">
-
-                            <h2>Citrus ale</h2>
-                            <h6>250ml (400kcal)</h6>
-                            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>$200</h4>
-                            <button>Add</button>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im10}></img>
-                        <div className="hehe">
-
-                            <h2>Cappucino</h2>
-                            <h6>Per Serve 160g (400kcal)</h6>
-                            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>$150</h4>
-                            <button>Add</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="Cards">
-                    <div className="info">
-                        <img src={im11}></img>
-                        <div className="hehe">
-
-                            <h2>Mexican Tacos</h2>
-                            <h6>Per Serve 200g (400kcal)</h6>
-                            <h6>amazing drink made up of dk something pls get it ok.i am sure you would love it ok pls huh i dont trust you but whatever</h6>
-                        </div>
-                        <div className="cart">
-                            <h4>$20</h4>
-                            <button>Add</button>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="Menu4">
+            {/* <div className="Menu4">
                 <div className="Cards">
                     <div className="info">
                         <img src={im22}></img>
@@ -657,7 +519,7 @@ function Order() {
 
                 </div>
 
-            </div>
+            </div> */}
 
 
             <Footer />
