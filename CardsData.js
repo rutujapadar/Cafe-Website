0763e6b4e89348fea1247159cf7883f884db741f
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-07-16 18:42:25 +0530

    Update CardsData.js

--- CardsData.js
+++ CardsData.js
@@ -2,27 +2,27 @@ const data=[
     {
        id:1,
        title:"Peach Iced Tea",
-       price:"₹150",
-       img:"../img/tang.JPG",
-       desc:"Incredibly simple 4-ingredient peach iced tea infused with real peaches and peach simple syrup. The perfect summer drink to sip by the pool.",
+       price:150,
+       img:"./img/tang.JPG",
+       desc:"Incredibly simple 4-ingredient peach iced tea infused with real peaches.",
        quantity:"250ml (400kcal)"
     },
 
-    {
+    {  
         id:2,
         title:"Cappucino",
-        price:"₹109",
-        img:"../img/cappu.JPG",
-        desc:"A cappuccino is an Italian coffee drink that is traditionally prepared with equal parts double espresso, steamed milk, and steamed milk foam on top.",
+        price:109,
+        img:"./img/cappu.JPG",
+        desc:"A cappuccino is an Italian coffee drink that is traditionally prepared ",
         quantity:"230ml (300kcal)"
      },
 
      {
         id:3,
         title:"Mocha",
-        price:"₹129",
-        img:"../img/mocha.JPG",
-        desc:"mocha is based on espresso and hot milk but with added chocolate flavouring and sweetener, typically in the form of cocoa powder and sugar.",
+        price:129,
+        img:"./img/mocha.JPG",
+        desc:"mocha is based on espresso and hot milk but with added chocolate flavour",
         quantity:"200ml (400kcal)"
      },
 
@@ -30,83 +30,84 @@ const data=[
      {
         id:4,
         title:"Americano",
-        price:"₹180",
-        img:"../img/americano.jpg",
-        desc:"The Americano is a popular espresso - based drink that originated in Italy. An Americano typically consists of equal parts espresso and hot water.",
-        quantity:"PER SERVE (22.5 ML) - 0 Kcal"
+        price:180,
+        img:"./img/americano.jpg",
+        desc:"The Americano is a popular espresso - based drink that originated in Italy.",
+        quantity:"PER SERVE (22.5 ML)"
      },
 
      {
         id:5,
-        title:"Banana Milkshake",
-        price:"₹170",
-        img:"../img/bananan.jpg",
-        desc:"A banana milkshake is a creamy and delicious beverage made by blending ripe bananas with milk and other Additional ingredients can include ice cream, yogurt ",
-        quantity:"PER SERVE (354ml) - 531 Kcal"
+        title:"Banana Shake",
+        price:170,
+        img:"./img/bananan.jpg",
+        desc:"A banana milkshake is delicious beverage made by mixing with milk  ",
+        quantity:"Per serve(354ml) "
      },
 
      {
       id:6,
-      title:"Blueberry Milkshake",
-      price:"₹199",
-      img:"../img/blueshake.jpeg",
-      desc:"A blueberry milkshake is a delightful and fruity beverage made by blending fresh or frozen blueberries with milk and Additional ingredients can include ice cream, yogurt",
-      quantity:"PER SERVE (354ml) - 535 Kcal"
+      title:"Bluesky Shake",
+      price:199,
+      img:"./img/blueshake.jpeg",
+      desc:"A blueberry milkshake is made by blending blueberries with milk ",
+      quantity:"Per serve(354ml)"
    },
 
 
    {
       id:7,
-      title:"Chocolate Shake",
-      price:"₹239",
-      img:"../img/chocoshake.jpeg",
-      desc:"A chocolate milkshake is a beloved beverage made by blending chocolate syrup, chocolate ice cream, or cocoa powder with milk to create a rich and creamy treat.",
-      quantity:"PER SERVE (354ml) - 500 Kcal"
+      title:"Choco Shake",
+      price:239,
+      img:"./img/chocoshake.jpeg",
+      desc:"A choco milkshake is made by mixing choco syrup, choco icecream",
+      quantity:"Per serve(354ml)"
    },
 
    {
       id:8,
       title:"Frappe",
-      price:"₹159",
-      img:"../img/frappe.jpg",
-      desc:"A frappe is a chilled, blended coffee or beverage that originated in Greece. It is typically made by mixing instant coffee with cold water, sugar, and ice, and then blending it until frothy.",
-      quantity:"PER SERVE (354ml) - 450 Kcal"
+      price:159,
+      img:"./img/frappe.jpg",
+      desc:"A frappe is a chilled, blended coffee that originated in Greece.",
+      quantity:"Per serve(354ml)"
    },
 
    {
       id:9,
       title:"Iced Tea",
-      price:"₹139",
-      img:"../img/icedtea.jpeg",
-      desc:"Iced tea is a refreshing beverage made by brewing tea and then cooling it with ice And contain Additional ingredients such as sweeteners (sugar, honey, or syrup), lemon juice.",
-      quantity:"Per Serve 150g (350kcal)"
+      price:139,
+      img:"./img/icedtea.jpeg",
+      desc:"Iced tea is a beverage made by brewing tea & cooling it with ice ",
+      quantity:"Per Serve (350ml)"
    }, 
    
    {
       id:10,
       title:"Latte",
-      price:"₹100",
-      img:"../img/latte.webp",
-      desc:"A latte is a popular coffee-based beverage that consists of espresso and steamed milk. Espresso is a concentrated form of coffee made by forcing hot water through finely ground coffee beans.",
-      quantity:"225ml (345kcal)"
+     
+      price:100,
+      img:"./img/latte.webp",
+      desc:"A latte is coffee-based beverage consists of espresso & steamed milk. ",
+      quantity:"per serve (225ml) "
    },
 
    {
       id:11,
-      title:"Red Velvet Shake",
-      price:"₹199",
-      img:"../img/redvelvet.jpeg",
-      desc:"A red velvet shake is a dessert beverage inspired by the popular red velvet cake. It combines the flavors of red velvet cake with the creamy texture of a milkshake. ",
-      quantity:"Per Serve 120g (200kcal)"
+      title:"Red dil Shake",
+      price:199,
+      img:"./img/redvelvet.jpeg",
+      desc:"A red dil shake is a dessert beverage inspired by the red velvet cake.  ",
+      quantity:"Per Serve 120g"
    },
 
 
    {
       id:12,
       title:"Mojito",
-      price:"₹159",
-      img:"../img/mojito.jpeg",
-      desc:"A mojito is a popular cocktail that originated in Cuba and has gained worldwide popularity. It is a refreshing and minty drink made with rum, lime juice, sugar, soda water, and fresh mint leaves.",
+      price:159,
+      img:"./img/mojito.jpeg",
+      desc:"A mojito is a mocktail originated in Cuba has gained worldwide fame",
       quantity:"Per Serve 120g (110kcal)"
    },
 
@@ -116,112 +117,112 @@ const data=[
    {
       id:13,
       title:"French Fries",
-      price:"₹159",
-      img:"../img/fries.jpeg",
-      desc:"French fries, also known as chips or fries and are re typically made by cutting potatoes into long, thin strips and deep-frying them until they become golden and crispy.",
-      quantity:"Per Serve 160g (400kcal)"
+      price:159,
+      img:"./img/fries.jpeg",
+      desc:"French fries, typically made by cutting potatoes & frying them ",
+      quantity:"Per Serve 160g "
    },
 
    {
       id:14,
       title:"Garlic Bread",
-      price:"₹179",
-      img:"../img/garlicbread.jpeg",
-      desc:"Garlic bread is a delicious and flavorful bread dish that is popular worldwide. It typically consists of sliced bread that is spread with a mixture of butter, minced garlic, and sometimes herbs.",
-      quantity:"Per Serve 160g (400kcal)"
+      price:179,
+      img:"./img/garlicbread.jpeg",
+      desc:"Garlic bread is a delicious bread dish with cheese & oregano.",
+      quantity:"Per Serve 160g"
    },
 
    {
       id:15,
       title:"Italian Pizza",
-      price:"₹299",
-      img:"../img/italian.jpeg",
-      desc:"Italian pizza is a globally renowned dish that originated in Italy and  characterized by a thin, crispy crust topped with various ingredients like dough made from flour, water, yeast, and salt.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:299,
+      img:"./img/italian.jpeg",
+      desc:"Italian pizza is a globally renowned dish with mushrooms & basil leaves.",
+      quantity:"Per Serve 200g"
    },
 
    {
       id:16,
       title:"Mexican Tacos",
-      price:"₹239",
-      img:"../img/taco.JPG",
-      desc:"Mexican tacos are a flavorful and popular dish that originated in Mexico. They consist of a tortilla, typically made from corn or wheat, filled with various ingredients and toppings.",
-      quantity:"250ml (400kcal)"
+      price:239,
+      img:"./img/taco.JPG",
+      desc:"Mexican tacos are a flavorful and popular dish that originated in Mexico. ",
+      quantity:"Per serve 250g"
    },
 
    {
       id:17,
-      title:"Macroni",
-      price:"₹159",
-      img:"../img/macaroi.jpeg",
-      desc:"Macaroni is a small, curved tube-shaped pasta commonly used in dishes like macaroni and cheese, pasta salads, soups.The ingredients of macaroni are durum wheat semolina, water.",
-      quantity:"Per Serve 160g (400kcal)"
+      title:"Macaroni",
+      price:159,
+      img:"./img/macaroi.jpeg",
+      desc:"Macaroni is small, curved tube-shaped pasta used in dishes",
+      quantity:"Per Serve 160g"
    },
 
 
    {
       id:18,
       title:"Margherita",
-      price:"₹250",
-      img:"../img/marghe.jpeg",
-      desc:"Margherita pizza is a classic Italian pizza.The main ingredients of a pizza are tomato sauce, mozzarella cheese, and fresh basil leaves. The crust is typically made from flour, water.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:250,
+      img:"./img/marghe.jpeg",
+      desc:"this is Italian pizza.main ingredients are tomato sauce, mozzarella cheese.",
+      quantity:"Per Serve 200g"
    },
 
    {
       id:19,
       title:"Pesto Sandwich",
-      price:"₹180",
-      img:"../img/pesto.jpg",
-      desc:"A pesto is a sandwich filled with pesto sauce, typically made from basil, garlic and olive oil and it is combined with ingredients like cheese, tomatoes, and vegetables for added flavor.",
-      quantity:"250ml (400kcal)"
+      price:180,
+      img:"./img/pesto.jpg",
+      desc:"A pesto is sandwich with pesto sauce made from basil, garlic & olive oil .",
+      quantity:"Per serve 250g"
    },
 
    {
       id:20,
-      title:"Mac And Cheese",
-      price:"₹150",
-      img:"../img/mac.jpg",
-      desc:"Mac and cheese, short for macaroni and cheese, is a popular comfort food dish made with cooked macaroni pasta and a creamy cheese sauce. It is a simple and satisfying dish.",
-      quantity:"Per Serve 160g (400kcal)"
+      title:"MacNCheese",
+      price:150,
+      img:"./img/mac.jpg",
+      desc:"macaroni & cheese made with macaroni pasta & cheese sauce",
+      quantity:"Per Serve 160g"
    },
 
    {
       id:21,
       title:"Toast",
-      price:"₹145",
-      img:"../img/toast.JPG",
-      desc:"Toast is a common breakfast food made by toasting slices of bread until they become crisp and golden. It is typically enjoyed with butter, jam, or other spreads and can be a versatile base.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:145,
+      img:"./img/toast.JPG",
+      desc:"Toast is a breakfast food made by toasting slices of bread with cheese",
+      quantity:"Per Serve 200g"
    },
 
    
    {
       id:22,
       title:"Aloopuff",
-      price:"₹150",
-      img:"../img/aloopuff.jpg",
-      desc:"Aloo puffs, also known as potato puffs, are a popular snack or appetizer in Indian cuisine. They consist of a savory filling made with spiced mashed potatoes, onions, and herbs.",
-      quantity:"250ml (400kcal)"
+      price:150,
+      img:"./img/aloopuff.jpg",
+      desc:"Aloo puffs/potato puffs, are a popular snack or appetizer in Indian cuisine.",
+      quantity:"Per serve 250g"
    },
 
    {
       id:23,
       title:"Classic Pizza",
-      price:"₹200",
-      img:"../img/classic.jpg",
-      desc:"Classic Pizza typically consists of a thin crust topped with tomato sauce, mozzarella cheese, and various toppings such as pepperoni, mushrooms, onions, or bell peppers.",
-      quantity:"Per Serve 160g (400kcal)"
+      price:200,
+      img:"./img/classic.jpg",
+      desc:"Classic Pizza,thin crust with tomato sauce, mozzarella cheese & toppings.",
+      quantity:"Per Serve 160g"
    },
 
 
    {
       id:24,
       title:"Pasta",
-      price:"₹180",
-      img:"../img/pasta.jpeg",
-      desc:"Pasta is a staple food made from durum wheat or semolina flour and water. Pasta is a versatile ingredient used in countless dishes, often served with sauces, vegetables.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:180,
+      img:"./img/pasta.jpeg",
+      desc:"Pasta is a staple food made from durum wheat/semolina flour & water.",
+      quantity:"Per Serve 200g"
    },
 
    
@@ -229,21 +230,21 @@ const data=[
    {
       id:25,
       title:"Cheesecake ",
-      price:"₹325",
-      img:"../img/c1.jpg",
-      desc:"Cheesecake is a rich and creamy dessert made with a crust, typically made from crushed biscuits or graham crackers, and a filling consisting of cream cheese, sugar.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:325,
+      img:"./img/cheesecake.JPG",
+      desc:"Made with a crust,from crushed biscuits or graham crackers & cheese.",
+      quantity:"Per Serve 200g"
    },
 
    
 
    {
       id:26,
-      title:"Crossiant",
-      price:"₹309",
-      img:"../img/croissant.JPG",
-      desc:"A croissant is a flaky and buttery pastry originating from France. It is a popular breakfast item enjoyed plain or filled with ingredients like ham, cheese, or chocolate.",
-      quantity:"Per Serve 160g (400kcal)"
+      title:"Croissant",
+      price:309,
+      img:"./img/croissant.JPG",
+      desc:"A croissant is a flaky and buttery pastry originating from France. ",
+      quantity:"Per Serve 160g"
    },
 
    
@@ -251,10 +252,10 @@ const data=[
    {
       id:27,
       title:"Donut",
-      price:"₹239",
-      img:"../img/donut.jpeg",
-      desc:"A donut is a sweet, fried pastry that typically has a round shape with a hole in the center. It is made from a dough mixture that is deep-fried until it becomes golden and puffy.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:239,
+      img:"./img/donut.jpeg",
+      desc:"A donut is sweet, fried pastry that has a round shape with a hole in center.",
+      quantity:"Per Serve 200g"
    },
 
    
@@ -262,10 +263,10 @@ const data=[
    {
       id:28,
       title:"Muffins",
-      price:"₹250",
-      img:"../img/muffins.jpeg",
-      desc:"Muffins are made from a batter consisting of flour, sugar, eggs, butter or oil, and baking powder. Muffins can be sweet or savory, with various flavorings such as chocolate chips, blueberries.",
-      quantity:"Per Serve 250g (400kcal)"
+      price:250,
+      img:"./img/muffins.jpeg",
+      desc:"Muffins are made from a batter of flour, sugar, eggs, butter.",
+      quantity:"Per Serve 250g"
    },
 
    
@@ -273,19 +274,19 @@ const data=[
    {
       id:29,
       title:"Pancake",
-      price:"₹159",
-      img:"../img/pancake.jpeg",
-      desc:"A pancake is a flat, round, and thin breakfast food made from a batter consisting of flour, eggs, milk, and a leavening agent such as baking powder. The batter is poured onto a hot griddle.",
-      quantity:"Per Serve 160g (400kcal)"
+      price:159,
+      img:"./img/pancake.jpeg",
+      desc:"A pancake is a flat,thin breakfast food made of batter of flour, eggs, milk.",
+      quantity:"Per Serve 160g"
    },
 
    {
       id:30,
       title:"Loafcake",
-      price:"₹199",
-      img:"../img/loafcake.jpg",
-      desc:"A loaf cake is a type of cake that is baked in a rectangular loaf pan.It typically has a dense and moist texture and can come in various flavors such as vanilla, chocolate, lemon, or banana.",
-      quantity:"Per Serve 200g (400kcal)"
+      price:199,
+      img:"./img/loafcake.jpg",
+      desc:"A loaf cake is a type of cake that is baked in a rectangular loaf pan.",
+      quantity:"Per Serve 200g"
    }
 
 ]
