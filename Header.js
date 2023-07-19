b776cea4bacc6c6a6baf2db6554fc1a80de4fd68
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-07-19 16:54:59 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -3,16 +3,15 @@ import logo from './img/logo.png';
 import order from './Order';
 import store from './Store';
 import { Link } from "react-router-dom";
-import About from "./About";
-import Home from './Home';
 import {useSelector} from "react-redux"
 // import {useDispatch} from "react-redux"
 // import {DLT} from "../redux/actions/action"
 import Badge from "@mui/material/Badge"
 import { styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import { ToastContainer,toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   '& .MuiBadge-badge': {
@@ -27,15 +26,23 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 }));
 
 function Header(){
- 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   const getData = useSelector((state)=>state.cartreducer.carts);
   console.log(getData);
 
-  const emptyCart = () => {
-    toast('Success Notification !', {
-        position: toast.POSITION.TOP_RIGHT
-    });
-};
+ 
     return(
 
   
@@ -67,13 +74,26 @@ function Header(){
     shopping_bag
     </span>
   </Link>:
- <Link  onClick={emptyCart}>
+  <>
+ <Link onClick={handleClick}>
   <span class="material-symbols-outlined" id="bag">
   shopping_bag
-  <ToastContainer/>
-  </span>
+  </span> 
   </Link>
-
+  <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>Empty Cart.</Typography>
+      </Popover>
+  
+</>
 
  
 }
