ec18b7ac7c4f84cb1415d0a3b5e3470b11221bee
vaishhpatil23 <137425612+vaishhpatil23@users.noreply.github.com>
2023-07-19 15:43:13 +0530

    Update Testimonials.js

--- Testimonials.js
+++ Testimonials.js
@@ -1,6 +1,6 @@
 import React from "react";
-import im1 from './img/cappu.JPG';
-import im2 from './img/taco.JPG';
+import im1 from './component/img/cappu.JPG';
+import im2 from './component/img/taco.JPG';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
@@ -8,7 +8,7 @@ function Testimonials(){
     return(
 
 	<div>
-    <h3 style={{marginTop:'21%',marginLeft:'6%',fontSize:'23px',fontFamily:'kanit',color:'#033539'}}>Our Reviews</h3>
+    <h3 style={{marginTop:'30%',marginLeft:'6%',fontSize:'23px',fontFamily:'kanit',color:'#033539'}}>Our Reviews</h3>
      <Carousel
         showArrows={true}
         infiniteLoop={true}
@@ -19,25 +19,23 @@ function Testimonials(){
       >
         <div className="testcon">
           <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+            <img src="https://cdn-icons-png.flaticon.com/128/6997/6997649.png" />
             <div className="myCarousel">
-              <h3>Shirley Fultz</h3>
-              <h4>Designer</h4>
+              <h3>John Sebastian</h3>
+              <br/>
               <p>
-                It's freeing to be able to catch up on customized news and not be
-                distracted by a social media element on the same site
+              This cozy cafe has left the best impressions! Hospitable hosts, delicious dishes, beautiful presentation, wonderful dessert.
               </p>
             </div>
           </div>
           <div><h1 style={{color:'white'}}>hh</h1></div>
           <div>
-          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+          <img src="https://cdn-icons-png.flaticon.com/128/6997/6997662.png" />
           <div className="myCarousel">
-            <h3>Shirley Feultz</h3>
-            <h4>Designer</h4>
+            <h3>isabella Grande</h3>
+           <br/>
             <p>
-              It's freeing to be able to catch up on customized news and not be
-              distracted by a social media element on the same site
+            It’s a great experience. The ambiance is very welcoming and charming. Amazing food and service. Staff are extremely knowledgeable.
             </p>
           </div>
         </div>
@@ -46,25 +44,23 @@ function Testimonials(){
 
         <div className="testcon">
           <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+            <img src="https://cdn-icons-png.flaticon.com/128/6997/6997649.png" />
             <div className="myCarousel">
               <h3>Shirley Fultz</h3>
-              <h4>Designer</h4>
+              <br/>
               <p>
-                It's freeing to be able to catch up on customized news and not be
-                distracted by a social media element on the same site
+              This place is great! Atmosphere is chill and cool but the staff is also really friendly.HIghly recommended!! 
               </p>
             </div>
           </div>
           <div><h1 style={{color:'white'}}>hh</h1></div>
           <div>
-          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+          <img src="https://cdn-icons-png.flaticon.com/128/6997/6997662.png" />
           <div className="myCarousel">
-            <h3>Shirley Feultz</h3>
-            <h4>Designer</h4>
+            <h3>Alice Walker</h3>
+           <br/>
             <p>
-              It's freeing to be able to catch up on customized news and not be
-              distracted by a social media element on the same site
+            Food is pretty good, some italian classics and some twists, and for their prices it’s 100% worth it.
             </p>
           </div>
         </div>
@@ -73,25 +69,23 @@ function Testimonials(){
 
         <div className="testcon">
           <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+            <img src="https://cdn-icons-png.flaticon.com/128/6997/6997662.png" />
             <div className="myCarousel">
-              <h3>Shirley Fultz</h3>
-              <h4>Designer</h4>
+              <h3>Angelica Hale</h3>
+              <br/>
               <p>
-                It's freeing to be able to catch up on customized news and not be
-                distracted by a social media element on the same site
+              I have to say, I enjoyed every single bite of the meal in Bella Italia. Considering the quality, the price is reasonable. 
               </p>
             </div>
           </div>
           <div><h1 style={{color:'white'}}>hh</h1></div>
           <div>
-          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+          <img src="https://cdn-icons-png.flaticon.com/128/6997/6997649.png" />
           <div className="myCarousel">
-            <h3>Shirley Feultz</h3>
-            <h4>Designer</h4>
+            <h3>Beatrix Potter</h3>
+           <br/>
             <p>
-              It's freeing to be able to catch up on customized news and not be
-              distracted by a social media element on the same site
+            Excellent food. Menu is extensive and seasonal to a particularly high standard. Definitely fine dining. It is worth trying!
             </p>
           </div>
         </div>
