48be8c9a6e975baaba3e25fb014cb5f4f1bc5e52
Rutuja Padar <115081556+rutujapadar@users.noreply.github.com>
2023-07-19 14:17:48 +0530

    Update CardsData.js

--- CardsData.js
+++ CardsData.js
@@ -1,11 +1,12 @@
 const data=[
     {
        id:1,
-       title:"Peach Iced Tea",
+       title:"Peach Tea",
        price:150,
        img:"./img/tang.JPG",
        desc:"Incredibly simple 4-ingredient peach iced tea infused with real peaches.",
-       quantity:"250ml (400kcal)"
+       quantity:"250ml (400kcal)",
+       qty:1
     },
 
     {  
@@ -14,7 +15,8 @@ const data=[
         price:109,
         img:"./img/cappu.JPG",
         desc:"A cappuccino is an Italian coffee drink that is traditionally prepared ",
-        quantity:"230ml (300kcal)"
+        quantity:"230ml (300kcal)",
+        qty:1
      },
 
      {
@@ -23,7 +25,8 @@ const data=[
         price:129,
         img:"./img/mocha.JPG",
         desc:"mocha is based on espresso and hot milk but with added chocolate flavour",
-        quantity:"200ml (400kcal)"
+        quantity:"200ml (400kcal)",
+        qty:1
      },
 
 
@@ -33,7 +36,8 @@ const data=[
         price:180,
         img:"./img/americano.jpg",
         desc:"The Americano is a popular espresso - based drink that originated in Italy.",
-        quantity:"PER SERVE (22.5 ML)"
+        quantity:"PER SERVE (22.5 ML)",
+        qty:1
      },
 
      {
@@ -42,7 +46,8 @@ const data=[
         price:170,
         img:"./img/bananan.jpg",
         desc:"A banana milkshake is delicious beverage made by mixing with milk  ",
-        quantity:"Per serve(354ml) "
+        quantity:"Per serve(354ml) ",
+        qty:1
      },
 
      {
@@ -51,7 +56,8 @@ const data=[
       price:199,
       img:"./img/blueshake.jpeg",
       desc:"A blueberry milkshake is made by blending blueberries with milk ",
-      quantity:"Per serve(354ml)"
+      quantity:"Per serve(354ml)",
+      qty:1
    },
 
 
@@ -61,7 +67,8 @@ const data=[
       price:239,
       img:"./img/chocoshake.jpeg",
       desc:"A choco milkshake is made by mixing choco syrup, choco icecream",
-      quantity:"Per serve(354ml)"
+      quantity:"Per serve(354ml)",
+      qty:1
    },
 
    {
@@ -70,7 +77,8 @@ const data=[
       price:159,
       img:"./img/frappe.jpg",
       desc:"A frappe is a chilled, blended coffee that originated in Greece.",
-      quantity:"Per serve(354ml)"
+      quantity:"Per serve(354ml)",
+      qty:1
    },
 
    {
@@ -79,7 +87,8 @@ const data=[
       price:139,
       img:"./img/icedtea.jpeg",
       desc:"Iced tea is a beverage made by brewing tea & cooling it with ice ",
-      quantity:"Per Serve (350ml)"
+      quantity:"Per Serve (350ml)",
+      qty:1
    }, 
    
    {
@@ -89,7 +98,8 @@ const data=[
       price:100,
       img:"./img/latte.webp",
       desc:"A latte is coffee-based beverage consists of espresso & steamed milk. ",
-      quantity:"per serve (225ml) "
+      quantity:"per serve (225ml) ",
+      qty:1
    },
 
    {
@@ -98,7 +108,8 @@ const data=[
       price:199,
       img:"./img/redvelvet.jpeg",
       desc:"A red dil shake is a dessert beverage inspired by the red velvet cake.  ",
-      quantity:"Per Serve 120g"
+      quantity:"Per Serve 120g",
+      qty:1
    },
 
 
@@ -108,7 +119,8 @@ const data=[
       price:159,
       img:"./img/mojito.jpeg",
       desc:"A mojito is a mocktail originated in Cuba has gained worldwide fame",
-      quantity:"Per Serve 120g (110kcal)"
+      quantity:"Per Serve 120g (110kcal)",
+      qty:1
    },
 
 
@@ -120,7 +132,8 @@ const data=[
       price:159,
       img:"./img/fries.jpeg",
       desc:"French fries, typically made by cutting potatoes & frying them ",
-      quantity:"Per Serve 160g "
+      quantity:"Per Serve 160g ",
+      qty:1
    },
 
    {
@@ -129,7 +142,8 @@ const data=[
       price:179,
       img:"./img/garlicbread.jpeg",
       desc:"Garlic bread is a delicious bread dish with cheese & oregano.",
-      quantity:"Per Serve 160g"
+      quantity:"Per Serve 160g",
+      qty:1
    },
 
    {
@@ -138,16 +152,18 @@ const data=[
       price:299,
       img:"./img/italian.jpeg",
       desc:"Italian pizza is a globally renowned dish with mushrooms & basil leaves.",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    },
 
    {
       id:16,
-      title:"Mexican Tacos",
+      title:"Tacos",
       price:239,
-      img:"./img/taco.JPG",
-      desc:"Mexican tacos are a flavorful and popular dish that originated in Mexico. ",
-      quantity:"Per serve 250g"
+      img:"./img/tacos.avif",
+      desc:"Mexican tacos are popular dish with tortilla & stuffings",
+      quantity:"Per serve 250g",
+      qty:1
    },
 
    {
@@ -156,7 +172,8 @@ const data=[
       price:159,
       img:"./img/macaroi.jpeg",
       desc:"Macaroni is small, curved tube-shaped pasta used in dishes",
-      quantity:"Per Serve 160g"
+      quantity:"Per Serve 160g",
+      qty:1
    },
 
 
@@ -166,16 +183,18 @@ const data=[
       price:250,
       img:"./img/marghe.jpeg",
       desc:"this is Italian pizza.main ingredients are tomato sauce, mozzarella cheese.",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    },
 
    {
       id:19,
-      title:"Pesto Sandwich",
+      title:"Pesto Sub",
       price:180,
       img:"./img/pesto.jpg",
       desc:"A pesto is sandwich with pesto sauce made from basil, garlic & olive oil .",
-      quantity:"Per serve 250g"
+      quantity:"Per serve 250g",
+      qty:1
    },
 
    {
@@ -184,7 +203,8 @@ const data=[
       price:150,
       img:"./img/mac.jpg",
       desc:"macaroni & cheese made with macaroni pasta & cheese sauce",
-      quantity:"Per Serve 160g"
+      quantity:"Per Serve 160g",
+      qty:1
    },
 
    {
@@ -193,7 +213,8 @@ const data=[
       price:145,
       img:"./img/toast.JPG",
       desc:"Toast is a breakfast food made by toasting slices of bread with cheese",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    },
 
    
@@ -203,7 +224,8 @@ const data=[
       price:150,
       img:"./img/aloopuff.jpg",
       desc:"Aloo puffs/potato puffs, are a popular snack or appetizer in Indian cuisine.",
-      quantity:"Per serve 250g"
+      quantity:"Per serve 250g",
+      qty:1
    },
 
    {
@@ -212,7 +234,8 @@ const data=[
       price:200,
       img:"./img/classic.jpg",
       desc:"Classic Pizza,thin crust with tomato sauce, mozzarella cheese & toppings.",
-      quantity:"Per Serve 160g"
+      quantity:"Per Serve 160g",
+      qty:1
    },
 
 
@@ -222,7 +245,8 @@ const data=[
       price:180,
       img:"./img/pasta.jpeg",
       desc:"Pasta is a staple food made from durum wheat/semolina flour & water.",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    },
 
    
@@ -233,7 +257,8 @@ const data=[
       price:325,
       img:"./img/cheesecake.JPG",
       desc:"Made with a crust,from crushed biscuits or graham crackers & cheese.",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    },
 
    
@@ -244,7 +269,8 @@ const data=[
       price:309,
       img:"./img/croissant.JPG",
       desc:"A croissant is a flaky and buttery pastry originating from France. ",
-      quantity:"Per Serve 160g"
+      quantity:"Per Serve 160g",
+      qty:1
    },
 
    
@@ -255,7 +281,8 @@ const data=[
       price:239,
       img:"./img/donut.jpeg",
       desc:"A donut is sweet, fried pastry that has a round shape with a hole in center.",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    },
 
    
@@ -266,7 +293,8 @@ const data=[
       price:250,
       img:"./img/muffins.jpeg",
       desc:"Muffins are made from a batter of flour, sugar, eggs, butter.",
-      quantity:"Per Serve 250g"
+      quantity:"Per Serve 250g",
+      qty:1
    },
 
    
@@ -277,7 +305,8 @@ const data=[
       price:159,
       img:"./img/pancake.jpeg",
       desc:"A pancake is a flat,thin breakfast food made of batter of flour, eggs, milk.",
-      quantity:"Per Serve 160g"
+      quantity:"Per Serve 160g",
+      qty:1
    },
 
    {
@@ -286,7 +315,8 @@ const data=[
       price:199,
       img:"./img/loafcake.jpg",
       desc:"A loaf cake is a type of cake that is baked in a rectangular loaf pan.",
-      quantity:"Per Serve 200g"
+      quantity:"Per Serve 200g",
+      qty:1
    }
 
 ]
